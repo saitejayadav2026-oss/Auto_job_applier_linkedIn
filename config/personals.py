85eca09d872ebc42a7d1bdeb2137d9b02b598a75
saitejayadav2026-oss <saitejayadav2026@gmail.com>
2026-05-18 00:33:10 -0500

    Update personal information in personals.py
    Updated personal information including author name, LinkedIn profile, and other personal details.

--- config/personals.py
+++ config/personals.py
@@ -1,8 +1,8 @@
 '''
-Author:     Sai Vignesh Golla
-LinkedIn:   https://www.linkedin.com/in/saivigneshgolla/
+Author:     Sri Sai Teja
+LinkedIn:   https://www.linkedin.com/in/sri-sai-23b9a1200
 
-Copyright (C) 2024 Sai Vignesh Golla
+Copyright (C) 2024 Sri Sai Teja
 
 License:    GNU Affero General Public License
             https://www.gnu.org/licenses/agpl-3.0.en.html
@@ -20,14 +20,14 @@ version:    2024.11.28.16.00
 
 # Your legal name
 first_name = "Sai"                 # Your first name in quotes Eg: "First", "Sai"
-middle_name = "Vignesh"            # Your name in quotes Eg: "Middle", "Vignesh", ""
-last_name = "Golla"                # Your last name in quotes Eg: "Last", "Golla"
+middle_name = ""            # Your name in quotes Eg: "Middle", "Vignesh", ""
+last_name = "Teja"                # Your last name in quotes Eg: "Last", "Golla"
 
 # Phone number (required), make sure it's valid.
-phone_number = "9876543210"        # Enter your 10 digit number in quotes Eg: "9876543210"
+phone_number = "9179001235"        # Enter your 10 digit number in quotes Eg: "9876543210"
 
 # What is your current city?
-current_city = ""                  # Los Angeles, San Francisco, etc.
+current_city = ""                  #
 '''
 Note: If left empty as "", the bot will fill in location of jobs location.
 '''
@@ -36,19 +36,19 @@ Note: If left empty as "", the bot will fill in location of jobs location.
 street = "123 Main Street"
 state = "STATE"
 zipcode = "12345"
-country = "Will Let You Know When Established"
+country = "United States"
 
 ## US Equal Opportunity questions
 # What is your ethnicity or race? If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
-ethnicity = "Decline"              # "Decline", "Hispanic/Latino", "American Indian or Alaska Native", "Asian", "Black or African American", "Native Hawaiian or Other Pacific Islander", "White", "Other"
+ethnicity = "Asian"              # "Decline", "Hispanic/Latino", "American Indian or Alaska Native", "Asian", "Black or African American", "Native Hawaiian or Other Pacific Islander", "White", "Other"
 
 # How do you identify yourself? If left empty as "", tool will not answer the question. However, note that some companies make compulsory to be answered
-gender = "Decline"                 # "Male", "Female", "Other", "Decline" or ""
+gender = "Male"                 # "Male", "Female", "Other", "Decline" or ""
 
 # Are you physically disabled or have a history/record of having a disability? If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
-disability_status = "Decline"      # "Yes", "No", "Decline"
+disability_status = "No"      # "Yes", "No", "Decline"
 
-veteran_status = "Decline"         # "Yes", "No", "Decline"
+veteran_status = "No"         # "Yes", "No", "Decline"
 ##
 
 
@@ -81,6 +81,6 @@ As an independent developer, I pour my heart and soul into creating tools like t
 Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
 
 Gratefully yours 🙏🏻,
-Sai Vignesh Golla
+Sai Teja
 '''
 ############################################################################################################
